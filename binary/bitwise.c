0196aa03b66bcb65b97e369da868bb9e6c11e5dd
cjwood032 <cjwood032@gmail.com>
2021-02-09 09:21:06 -0500

    added shifts

--- binary/bitwise.c
+++ binary/bitwise.c
@@ -14,5 +14,13 @@ int main ()
     printf ("~w1 is: %d\n",w3);
     w3=~w2;
     printf ("~w2 is: %d\n",w3);
+    w3 = w1<<1;
+    printf ("w1 left shifted 1 is: %d\n",w3);
+    w3 = w1>>1;
+    printf ("w1 right shifted 1 is: %d\n",w3);
+    w3 = w2<<1;
+    printf ("w2 left shifted 1 is: %d\n",w3);
+    w3 = w2>>1;
+    printf ("w2 right shifted 1 is: %d\n",w3);
     return 0;
 }
