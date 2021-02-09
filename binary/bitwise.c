72b8ac2f6796a8db239794cc1551a5e4e7f094a6
cjwood032 <cjwood032@gmail.com>
2021-02-09 09:10:36 -0500

    bitwise example

---
+++ binary/bitwise.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () 
+{
+    short int w1 = 25; //0000000000011001
+    short int w2 = 77; //0000000001001101
+    short int w3 = 0;
+    w3=w1&w2;
+    printf ("& is: %d\n",w3);
+    w3 = w1 | w2;
+    printf ("| is: %d\n",w3);
+    w3 = w1^ w2;
+    printf ("^ is: %d\n",w3);
+    w3 = ~w1;
+    printf ("~w1 is: %d\n",w3);
+    w3=~w2;
+    printf ("~w2 is: %d\n",w3);
+    return 0;
+}
