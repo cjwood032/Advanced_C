19f5ca7f65c6cac394b5f3f25bdc08ee6fd903a5
cjwood032 <cjwood032@gmail.com>
2021-02-09 11:57:06 -0500

    added comma op examples

---
+++ comma/comma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{/*
+    while (i<100)
+        sum+=data[i], ++i;
+    int i(5,10);//10 is assigned to i
+    int j = (f1(),f2());
+    x = (y=3, (z=++y +2)++5); x will = 11
+    for (i=0, j=1; i!=10; i++, j-=10)
+    */
+    return 0;
+}
