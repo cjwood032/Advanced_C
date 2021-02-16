14663cd96e63dc1f509728faf114294837238e7f
cjwood032 <cjwood032@gmail.com>
2021-02-16 10:29:51 -0500

    trees

---
+++ trees/example.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+struct treeNode {
+    int data;
+    struct treeNode *leftPtr;
+    struct treeNode *rightPtr;
+};
+typedef struct treeNode TreeNode;
+typedef TreeNode *TreeNodePtr;
+
+void insertNode(TreeNodePtr *treePtr, int value);
+void inOrder(TreeNodePtr treePtr);
+void preOrder (TreeNodePtr treePtr);
+void postOrder (TreeNodePtr treePtr);
+
+
+int main()
+{
+    int i = 0;
+    int item = 0;
+    TreeNodePtr rootPtr=NULL;
+    srand(time(NULL));
+    printf("Tree numbers\n");
+    for (i=1; i<=10;i++)
+    {
+        item = rand() %15;
+        printf("%3d", item);
+        insertNode(&rootPtr, item);
+    }
+    printf("\npreorder: ");
+    preOrder(rootPtr);
+    printf("\ninorder: ");
+    inOrder(rootPtr);
+    printf("\npostorder: ");
+    postOrder(rootPtr);
+    return 0;
+}
+void insertNode(TreeNodePtr *treePtr, int value)
+{
+    if (*treePtr == NULL)
+    {
+        *treePtr = malloc(sizeof(TreeNode));
+
+        if (*treePtr != NULL)
+        {
+            (*treePtr)->data = value;
+            (*treePtr)->leftPtr = NULL;
+            (*treePtr)->rightPtr = NULL;
+        }
+        else
+        {
+            printf("not enough mem\n");
+        }
+    }
+    else
+    {
+        if(value<(*treePtr)->data)
+        {
+            insertNode( &((*treePtr)->leftPtr),value);
+        }
+        else if(value>(*treePtr)->data)
+        {
+            insertNode( &((*treePtr)->rightPtr),value);
+        }
+        else
+        {
+            printf(" %d is a duplicate value\n",value);
+        }
+    }
+}
+void inOrder(TreeNodePtr treePtr)
+{
+    if(treePtr!=NULL)
+    {
+        inOrder(treePtr->leftPtr);
+        printf("%3d",treePtr->data);
+        inOrder(treePtr->rightPtr);
+    }
+}
+void preOrder(TreeNodePtr treePtr)
+{
+    if(treePtr!=NULL)
+    {
+        printf("%3d",treePtr->data);
+        preOrder(treePtr->leftPtr);
+        preOrder(treePtr->rightPtr);
+    }
+}
+void postOrder(TreeNodePtr treePtr)
+{
+    if(treePtr!=NULL)
+    {
+        postOrder(treePtr->leftPtr);
+        postOrder(treePtr->rightPtr);
+        printf("%3d",treePtr->data);
+    }
+}
